72985ab7ea91e511079f906acd80fd43bbb32ee3
ShaunLWM <shaunlimweimin@gmail.com>
2021-06-19 00:59:35 +0800

    Misc

--- src/screens/WalletScreen.tsx
+++ src/screens/WalletScreen.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import {ScrollView} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {CollapsibleHeaderSectionList} from "react-native-collapsible-header-views";
+import {getStatusBarHeight} from "react-native-status-bar-height";
 import BigHeader from "../common/BigHeader";
-import styled from "styled-components/native";
-
-const StyledScrollView = styled.ScrollView.attrs(() => ({
-	contentContainerStyle: {
-		paddingHorizontal: 12,
-	},
-}))``;
 
 export default function WalletScreen() {
 	return (
-		<SafeAreaView>
-			<StyledScrollView>
-				<BigHeader text="Wallet" />
-			</StyledScrollView>
-		</SafeAreaView>
+		<CollapsibleHeaderSectionList
+			CollapsibleHeaderComponent={<BigHeader text="Wallet" />}
+			headerHeight={100}
+			statusBarHeight={getStatusBarHeight()}
+			headerContainerBackgroundColor={"green"}
+			data={data}
+			renderItem={Item}
+			ItemSeparatorComponent={Separator}
+			keyExtractor={keyExtractor}
+			sections={["Latest", "Addresses"]}
+		/>
 	);
 }
