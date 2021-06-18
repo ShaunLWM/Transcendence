8d44b46c59a74ea0a93897a4571deefcc0d05ca1
ShaunLWM <shaunlimweimin@gmail.com>
2021-06-18 23:36:16 +0800

    Add redux flipper middleware to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,18 @@
 import {configureStore} from "@reduxjs/toolkit";
 import coingeckoReducer from "./slices/CoinGecko";
 
+const middlewares = [];
+
+if (__DEV__) {
+	const createDebugger = require("redux-flipper").default;
+	middlewares.push(createDebugger());
+}
+
 const store = configureStore({
 	reducer: {
 		coingecko: coingeckoReducer,
 	},
+	middleware: middlewares,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
