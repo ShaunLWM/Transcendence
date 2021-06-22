79ad0cbddf5aee6186833ae028cb2b5513846ed8
ShaunLWM <shaunlimweimin@gmail.com>
2021-06-23 00:17:27 +0800

    Add AddWalletModal

--- src/common/modals/AddWalletModal.tsx
+++ src/common/modals/AddWalletModal.tsx
@@ -1,8 +1,82 @@
-import React from "react";
+import {Picker} from "@react-native-picker/picker";
+import {useNavigation} from "@react-navigation/native";
+import React, {useMemo, useState} from "react";
+import {Button} from "react-native";
+import {OutlinedTextField} from "rn-material-ui-textfield";
 import styled from "styled-components/native";
+import {IWallet} from "../../models/WalletSchema";
+import {PriceKey, PriceKeysValue} from "../../store/slices/CoinGecko";
+import {getRealm, isValidAddress} from "../../utils/Helper";
 
-const Container = styled.View``;
+const Container = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Content = styled.View`
+	width: 80%;
+`;
 
 export default function AddWalletModal() {
-	return <Container />;
+	const navigation = useNavigation();
+	const [name, setName] = useState("");
+	const [nameError, setNameError] = useState("");
+
+	const [address, setAddress] = useState("");
+	const [addressError, setAddressError] = useState("");
+
+	const [selectedToken, setSelectedToken] = useState<PriceKey>("bnb");
+
+	const onSubmit = async () => {
+		const realm = await getRealm();
+		try {
+			realm.write(() => {
+				realm.create<IWallet>("WalletItem", {address, name, type: "bnb"});
+				navigation.goBack();
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const onChangeAddress = (str: string) => {
+		setAddress(str);
+		// TODO: check db for duplicate address
+		if (!isValidAddress("bnb", str) && str.length > 0) {
+			setAddressError("Invalid address");
+		} else {
+			setAddressError("");
+		}
+	};
+
+	const onChangeName = (str: string) => {
+		setName(str);
+		// TODO: check db for duplicate name
+	};
+
+	const submitDisabled = useMemo(() => {
+		return !(name.length > 0 && address.length > 0 && addressError.length < 1 && nameError.length < 1);
+	}, [address, addressError, name, nameError]);
+
+	return (
+		<Container>
+			<Content>
+				<OutlinedTextField label="Wallet Name" onChangeText={onChangeName} value={name} error={nameError} />
+				<OutlinedTextField label="Address" onChangeText={onChangeAddress} value={address} error={addressError} />
+				<Picker selectedValue={selectedToken} onValueChange={setSelectedToken}>
+					{PriceKeysValue.map(v => (
+						<Picker.Item key={v} label={v.toUpperCase()} value={v} />
+					))}
+				</Picker>
+				<Button
+					onPress={onSubmit}
+					title="Add"
+					color="#841584"
+					accessibilityLabel="Add Wallet"
+					disabled={submitDisabled}
+				/>
+			</Content>
+		</Container>
+	);
 }
